307c1ae732d92f366f1102df6128af28f668da1e
315tky <315tky@gmail.com>
2020-02-26 18:23:45 +0900

    fixed curl flags

--- eve_mysql_data_import/fuzzwork_mysql_sde_download.sh
+++ eve_mysql_data_import/fuzzwork_mysql_sde_download.sh
@@ -4,6 +4,7 @@ md5_file="mysql-latest.tar.bz2.md5"
 mysql_file="mysql-latest.tar.bz2"
 md5_url="https://www.fuzzwork.co.uk/dump/mysql-latest.tar.bz2.md5"
 mysql_url="https://www.fuzzwork.co.uk/dump/mysql-latest.tar.bz2"
+curl_flags="--silent --show-error --output"
 
 if [ -f $md5_file ];then
   rm $md5_file
@@ -11,11 +12,11 @@ fi
 
 # get latest md5_file
 echo "downloading latest md5sum for check"
-curl --output ./$md5_file $md5_url
+curl $curl_flags ./$md5_file $md5_url
 
 if [ ! -f $mysql_file ];then 
   echo "no mysql tar file found, downloading new"
-  curl --output ./$mysql_file $mysql_url
+  curl $curl_flags ./$mysql_file $mysql_url
   latest_md5_checksum=`cat $md5_file`
   mysql_file_checksum=`/usr/bin/md5sum $mysql_file`
   if [ "$latest_md5_checksum" = "$mysql_file_checksum" ];then  
@@ -33,7 +34,7 @@ else
     echo "latest md5sum file matches previous mysql tar file, nothing to do"
     exit
   else
-    curl --output ./$mysql_file $mysql_url
+    curl $curl_flags ./$mysql_file $mysql_url
     mysql_file_checksum=`/usr/bin/md5sum $mysql_file`
     if [ "$latest_md5_checksum" = "$mysql_file_checksum" ];then
       tar xvfj $mysql_file
